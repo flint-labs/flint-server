0deddb4698f6ba867533be51454d037536da86b3
wallypark710 <wallypark710@gmail.com>
2019-03-05 17:06:45 +0900

    Edit async await logic
     - try-catch 를 통한 에러 핸들링

--- src/route/api/history/controller.js
+++ src/route/api/history/controller.js
@@ -1,14 +1,22 @@
 const { Challenges } = require('../../../models');
+const { getLogger } = require('../../../../config');
+
+const logger = getLogger('Challenges');
 
 // GET /api/history/completeList/:userId
 exports.completeList = async (req, res) => {
   const { userId } = req.params;
-  const completeChallengelist = await Challenges.findAll({
-    where: {
-      userId,
-      $or: [{ state: { $eq: 'success' } }, { state: { $eq: 'faliure' } }],
-    },
-  });
 
-  res.status(200).send(completeChallengelist);
+  try {
+    const completeChallengelist = await Challenges.findAll({
+      where: {
+        userId,
+        $or: [{ state: { $eq: 'success' } }, { state: { $eq: 'faliure' } }],
+      },
+    });
+    return res.status(200).send(completeChallengelist);
+  } catch (err) {
+    logger.error(err);
+    return res.status(400).send({ err });
+  }
 };
