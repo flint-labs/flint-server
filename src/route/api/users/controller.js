c4c650ec7d5a6fb90de658c183106256ab2b655e
wallypark710 <wallypark710@gmail.com>
2019-03-06 18:05:02 +0900

    Add status data

--- src/route/api/users/controller.js
+++ src/route/api/users/controller.js
@@ -1,5 +1,7 @@
-const { Users } = require('../../../models');
-const { Joi: { validateUser } } = require('../../../service');
+const { Users, Challenges } = require('../../../models');
+const {
+  Joi: { validateUser },
+} = require('../../../service');
 const { encryptPassword } = require('../../../lib/util');
 const { getLogger } = require('../../../../config');
 
@@ -12,8 +14,26 @@ exports.getDetail = async (req, res) => {
     const user = await Users.findOne({ where: { id } });
     if (!user) return res.status(400).send('등록되지 않은 사용자입니다 :(');
 
+    const list = await Challenges.findAll({ where: { userId: id } });
+    const totalChallenges = list.length;
+    let inProgress = 0;
+    let success = 0;
+
+    list.forEach(ele => {
+      if (ele.dataValues.state === 'inProgress') {
+        inProgress += 1;
+      }
+      if (ele.dataValues.state === 'success') {
+        success += 1;
+      }
+    });
+
     const { dataValues } = user;
     delete dataValues.password;
+
+    dataValues.totalChallenges = totalChallenges;
+    dataValues.inProgress = inProgress;
+    dataValues.success = success;
 
     return res.status(200).send({ user: dataValues });
   } catch (error) {
@@ -51,15 +71,21 @@ exports.signUp = async (req, res) => {
   try {
     const { user } = req.body;
     const { value, error } = validateUser(user);
-    if (error) return res.status(400).send('입력하신 양식이 맞지 않습니다 :(');
+    if (error) {
+      return res.status(400).send('입력하신 양식이 맞지 않습니다 :(');
+    }
 
     const { email } = value;
     const emailExist = await Users.findOne({ where: { email } });
-    if (emailExist) return res.status(400).send('이미 사용된 이메일입니다 :(');
+    if (emailExist) {
+      return res.status(400).send('이미 사용된 이메일입니다 :(');
+    }
 
     const { nickname } = value;
     const nicknameExist = await Users.findOne({ where: { nickname } });
-    if (nicknameExist) return res.status(400).send('이미 사용된 닉네임입니다 :(');
+    if (nicknameExist) {
+      return res.status(400).send('이미 사용된 닉네임입니다 :(');
+    }
 
     const { password } = value;
     const encrypted = await encryptPassword(password);
