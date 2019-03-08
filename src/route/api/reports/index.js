df99dc59b7e072a9957fc35171869fb859d672a4
wallypark710 <wallypark710@gmail.com>
2019-03-08 11:42:24 +0900

    Apply token check module on reports router
     - 토큰 유효성 검사 모듈 적용

--- src/route/api/reports/index.js
+++ src/route/api/reports/index.js
@@ -1,14 +1,16 @@
 const route = require('express').Router();
 const controller = require('./controller');
+const { checkToken } = require('../../../lib/middleware');
+const checkAccessToken = checkToken('x-access-token');
 
 route.post('/postReport', controller.postReport);
 
-route.post('/responseReport', controller.responseReport);
+route.post('/responseReport', checkAccessToken, controller.responseReport);
 route.get(
   '/getNotPendingReports/:challengeId',
   controller.getNotPendingReports,
 );
 
-route.get('/getRequireList/:id', controller.getRequireList);
+route.get('/getRequireList/:id', checkAccessToken, controller.getRequireList);
 
 module.exports = route;
