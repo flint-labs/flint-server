3b7d5a597901765b4a33a4b085e36e1da9e84b32
sunjae-kim <kimsj9484@gmail.com>
2019-03-11 15:20:52 +0900

    Modify challengeSetting route
     - Remove handling properties logic

--- src/route/api/challenges/controller.setting.js
+++ src/route/api/challenges/controller.setting.js
@@ -2,57 +2,18 @@ const { Challenges, Users, Charities } = require('../../../models');
 
 // POST /api/challenges/setting
 exports.challengeSetting = async (req, res) => {
-  const {
-    title,
-    startYear,
-    startMonth,
-    startDay,
-    week,
-    checkingPeriod,
-    amount,
-    isOnGoing,
-    slogan,
-    referee,
-    userId,
-    selectUser,
-    selectCharity,
-    category,
-  } = req.body;
-
-  const startAt = new Date(startYear, startMonth - 1, startDay);
-  const endAt = new Date(startAt.getTime() + 604800000 * week);
-  const state = startAt.getTime() - Date.now() > 0 ? 'pending' : 'inProgress';
+  const { challenge } = req.body;
 
   const refereeId = await Users.findOne({
-    where: { nickname: referee },
-    attributes: ['id'],
-  });
-  const receipientUserId = await Users.findOne({
-    where: { nickname: selectUser },
+    where: { nickname: challenge.referee },
     attributes: ['id'],
   });
 
-  console.log(req.body);
+  challenge.refereeId = refereeId.dataValues.id;
+
+  delete challenge.referee;
   try {
-    await Challenges.create({
-      title,
-      userId,
-      startAt,
-      endAt,
-      category,
-      state,
-      isOnGoing,
-      slogan,
-      refereeId: refereeId ? refereeId.dataValues.id : userId,
-      checkingPeriod: Number(checkingPeriod),
-      amount: Number(amount.split(',').join('')),
-      receipient_user_id: receipientUserId
-        ? receipientUserId.dataValues.id
-        : null,
-      receipient_charity_id: selectCharity || null,
-      description: 'undefined',
-    });
-
+    await Challenges.create(challenge);
     res.status(200).send('ok');
   } catch (err) {
     console.log(err.message);
